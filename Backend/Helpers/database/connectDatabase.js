8a862868473f3f0b997a01498336d3a0411b0b8d
SawtantarSingh <91089675+SawtantarSingh@users.noreply.github.com>
2023-11-02 20:57:59 +0530

    Update connectDatabase.js

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 
 const connectDatabase =async  () => {
 
-    await mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser : true})
+    await mongoose.connect(process.env.MONGO_URI ,{
+      useNewUrlParser: "true",
+      useUnifiedTopology: "true",                                     
+        })
 
     console.log("MongoDB Connection Successfully")
 
